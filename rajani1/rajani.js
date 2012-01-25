46af8fd9c1825751b6389e887bf9c033b8dd5b73
mark.dawn@gmail.com <mark.dawn@gmail.com>
2012-01-25 18:22:32 +0530

    just some update

--- rajani1/rajani.js
+++ rajani1/rajani.js
@@ -1,1 +1,16 @@
-alert('hello');
+var t=true;
+window.addEventListener('deviceorientation', function(event) {
+  var a = event.alpha;
+  var b = event.beta;
+  var g = event.gamma;
+  if(t){
+  	console.log(event);
+  	t=false;	
+  }
+  
+  var el=document.querySelector('#rajani')
+  if(el){
+  	var style=el.style;	
+  	style.webkitTransform = 'rotateZ(' + (-1.0 * g) + 'deg)';
+  }
+}, false);
